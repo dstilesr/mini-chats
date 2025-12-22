5630fde3411b3176e295a15c5741a22be2f724bb
dstilesr <d.stiles.r@hotmail.com>
2025-12-22 11:55:56 -0500

    Dockerfile for python version

--- python-src/src/app/settings.py
+++ python-src/src/app/settings.py
@@ -4,9 +4,6 @@ from typing import Literal
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-# Default: assets folder in the repository root
-_default_static = Path(__file__).parents[3] / "assets"
-
 
 class AppSettings(BaseSettings):
     """
@@ -16,7 +13,10 @@ class AppSettings(BaseSettings):
     environment: str = Field(default="dev")
     port: int = Field(default=3501, gt=0)
     version: str = Field(default="v0.1.0")
-    static_path: Path = Field(default=_default_static)
+    static_path: Path = Field(
+        default_factory=lambda: Path(__file__).parents[3] / "assets",
+        description="Path from which to serve static files",
+    )
     log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = Field(
         default="INFO"
     )
